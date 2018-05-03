a41b55fcd102e0173e210e86a9c4655bfb817646
Mary Han <maryzhenghan@gmail.com>
2018-05-03 17:09:56 -0400

    Add Heroku link to config.js

--- config.js
+++ config.js
@@ -1,5 +1,5 @@
 'use strict';
 
-exports.DATABASE_URL = process.env.DATABASE_URL || 'mongodb://localhost/migraine-app';
+exports.DATABASE_URL = process.env.DATABASE_URL || 'https://stormy-temple-33929.herokuapp.com/';
 exports.TEST_DATABASE_URL = process.env.TEST_DATABASE_URL || 'mongodb://localhost/test-migraine-app';
 exports.PORT = process.env.PORT || 8080;
