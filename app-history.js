0a38f503362a08efc8cd5d9cb72f00e2564c9f05
Mary Han <maryzhenghan@gmail.com>
2018-05-21 14:57:02 -0400

    Fixed whitespace for JSON on history.html page

--- app-history.js
+++ app-history.js
@@ -99,10 +99,10 @@ function getAllLogs(callbackFn) {
 // function stays same when connecting to real API later
 function displayAllLogs(data) {
 	for (index in data.allLogs) {
-		let simpJson = JSON.stringify(data.allLogs[index], null, 10);
+		let simpJson = JSON.stringify(data.allLogs[index], null, "\t");
 
 		$('body').append(
-			'<p>' + data.allLogs[index]['entry date'] + ': ' + data.allLogs[index].migraine + ' migraine' + '</p><p>' + simpJson + '</p>');
+			'<p>' + data.allLogs[index]['entry date'] + ': ' + data.allLogs[index].migraine + ' migraine' + '</p><p class="js-simpJson">' + simpJson + '</p>');
 	}
 }
 
