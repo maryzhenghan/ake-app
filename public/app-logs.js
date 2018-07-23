4ab19aa861ba3071bec4ae1b282c8bce30575daa
Mary Han <maryzhenghan@gmail.com>
2018-07-23 16:46:20 -0700

    Set up GET request for all logs page

--- public/app-logs.js
+++ public/app-logs.js
@@ -1,19 +1,30 @@
 function getAllLogs(callbackFn) {
-	setTimeout(function(){ callbackFn(MOCK_ALL_LOGS)}, 100);
+	let settings = {
+		url: '/logs',
+		method: 'GET'
+	};
+
+	$.ajax(settings)
+	.done(data => {
+		callbackFn(data);
+	});
 }
 
-// function stays same when connecting to real API later
 function displayAllLogs(data) {
-	for (index in data.allLogs) {
-		let simpJson = JSON.stringify(data.allLogs[index], null, "\t");
 
-		$('body').append(
-			'<p>' + data.allLogs[index]['entry date'] + ': ' + data.allLogs[index].migraine + ' migraine' + '</p><p class="js-simpJson">' + simpJson + '</p>');
+	for (let i in data.logs) {
+		let simpJson = JSON.stringify(data.logs[i], null, "\t");
+
+		$('.js-allLogs').append(
+			`<p>${data.logs[i].dateAdjusted}:</p>
+			<p>${simpJson}</p>
+			<hr>
+			`);
 	}
+
+
 }
 
-
-// function stays the same even when connecting to real API
 function getDisplayLogs() {
 	getAllLogs(displayAllLogs);
 }
