3b96d32fbcededc0f88829da961d1265be1ec41f
Mary Han <maryzhenghan@gmail.com>
2018-07-31 09:55:08 -0700

    Swap PUT request semantics

--- server.js
+++ server.js
@@ -161,7 +161,7 @@ app.put('/logs/:id', (req, res) => {
 
 	Log
 		.findByIdAndUpdate(req.params.id, { $set: toUpdate })
-		.then(log => res.status(204).json())
+		.then(log => res.status(204).end())
 		.catch(err => {
 			console.error(err);
 			res.status(500).json({ message: 'Internal server error' });
