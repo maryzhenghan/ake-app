9a050be4a5d93a7bd1479973f3911697891b8d79
Mary Han <maryzhenghan@gmail.com>
2018-08-02 11:17:40 -0700

    Update db URL to Heroku link

--- config.js
+++ config.js
@@ -1,5 +1,5 @@
 'use strict';
 
-exports.DATABASE_URL = process.env.DATABASE_URL || 'mongodb://testuser1:testpw1@ds215380.mlab.com:15380/migraine-app';
+exports.DATABASE_URL = process.env.DATABASE_URL || 'https://stormy-temple-33929.herokuapp.com/';
 exports.TEST_DATABASE_URL = process.env.TEST_DATABASE_URL || 'mongodb://localhost/test-migraine-app';
 exports.PORT = process.env.PORT || 8080;
