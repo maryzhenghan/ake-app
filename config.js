99016cdb6f42e5397b8363b176a7fb0ec2a27e33
Mary Han <maryzhenghan@gmail.com>
2018-08-16 14:53:25 -0700

    Update config.js database URL

--- config.js
+++ config.js
@@ -1,7 +1,7 @@
 'use strict';
 
 exports.DATABASE_URL =
-  process.env.DATABASE_URL || 'mongodb://testuser1:testpw1@ds215380.mlab.com:15380/migraine-app';
+  process.env.DATABASE_URL || 'https://ake-app.herokuapp.com/';
 exports.TEST_DATABASE_URL =
   process.env.TEST_DATABASE_URL || 'mongodb://localhost/test-migraine-app';
 exports.PORT = process.env.PORT || 8080;
