c261511f1e056c29b3ef3060c2dee020ae247e0b
Mary Han <maryzhenghan@gmail.com>
2018-08-16 13:10:23 -0700

    Update app name

--- config.js
+++ config.js
@@ -1,7 +1,7 @@
 'use strict';
 
 exports.DATABASE_URL =
-  process.env.DATABASE_URL || 'https://ake-app.herokuapp.com/';
+  process.env.DATABASE_URL || 'https://stormy-temple-33929.herokuapp.com/';
 exports.TEST_DATABASE_URL =
   process.env.TEST_DATABASE_URL || 'mongodb://localhost/test-migraine-app';
 exports.PORT = process.env.PORT || 8080;
